caf30b81b6f8385641c99f5fa438f062dcb09293
jose.saraceni <jose.vitor08@outlook.com>
2024-10-04 17:09:36 -0300

    first commit

---
+++ src/scripts/engine.js
@@ -0,0 +1,41 @@
+const emojis = [
+    "😁",
+    "😁",
+    "😒",
+    "😒",
+    "👌",
+    "👌",
+    "💕",
+    "💕",
+    "🤣",
+    "🤣",
+    "🗺️",
+    "🗺️"
+];
+
+let openCards =[];
+
+let shufflEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1 ));
+
+for(let i=0; i < emojis.length; i++){
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shufflEmojis[i];
+    box.onclick = handleClick;
+    document.querySelector(".game").appendChild(box);
+}
+
+function handleClick(){
+    if(openCards.length < 2 ){
+        this.classList.add("boxOpen");
+        openCards.push(this);
+    }
+
+    if(openCards.length == 2 ){
+        setTimeout(checkMatch,500);
+    }
+}
+
+function checkMatch(){
+    
+}
